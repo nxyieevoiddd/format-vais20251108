c30376736edec3068bed44d58f87f4565231b257
User <nikolajevs04@gmail.com>
2025-11-08 13:23:16 +0200

    gatavs

---
+++ scriptv2.js
@@ -0,0 +1,20 @@
+// 1.uzdevums
+// Izveidojiet primitīvu HTML un JS aplikāciju, kas lietotājam ļauj aprēķināt rīņķa līnijas garumu vai laukumu pēc lietotāja ievadītā riņķa rādiusa.
+// !! Izmantojiet tikai devdocs.io !!
+// devdocs.io nepieciešams iepējot (enable) sadaļu "Web APIs", jo ar Javascript sadaļu vien nepietiks.
+// Atslēgas vārdi: document, addEventListener, value, getElementById, innerHTML
+
+// 2.uzdevums
+// Izveidojiet primitīvu HTML un JS kalkulātora aplikāciju, kas lietotājam ļauj sareizināt un izdalīt divus skaitļus.
+console.log(Output);
+const inputs = document.getElementById("numberInput");
+const calcuButton = document.getElementById("calcuButton");
+const outPut = document.getElementById("outPut");
+calcButton.addEventListener("click", clickHandle);
+function clickHandle() {
+   input = a+ b 
+   input = a/b
+   input = a*b
+
+}
+console.log(input)
